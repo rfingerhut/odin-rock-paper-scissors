22c7f2ff77c00df4ca7669d1be3f25ead360ae0d
Rachel <24816855+rfingerhut@users.noreply.github.com>
2025-10-15 19:43:16 -0700

    Add DOM method for winner of round

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const resultBox = document.querySelector("#results");
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorBtn = document.querySelector('#scissors');
+
+rockBtn.addEventListener('click', () => {
+    playRound('rock', getComputerChoice());
+});
+
+paperBtn.addEventListener('click', () => {
+    playRound('paper', getComputerChoice())
+});
+scissorBtn.addEventListener('click', () => {
+    playRound('scissors', getComputerChoice());
+});
+
 function getComputerChoice(){
     let choice = Math.random();
     if (choice < 1/3){
@@ -18,42 +34,41 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
+    let winner = '';
     console.log("Human chooses: " + humanChoice);
     console.log("Computer chooses: " + computerChoice);
     if (humanChoice == computerChoice){
-        console.log("Tie!")
+        console.log("Tie!");
+        winner = 'Tie!';
     } else if (humanChoice == "rock" && computerChoice != "paper"){
         console.log("Human won.");
         humanScore++;
+        winner = 'Human';
     } else if (humanChoice == "paper" && computerChoice != "scissors"){
         console.log("Human won.");
         humanScore++;
+        winner = 'Human';
     } else if (humanChoice == "scissors" && computerChoice != "rock"){
         console.log("Human won.");
         humanScore++;
+        winner = 'Human';
     } else {
         console.log("Computer won.");
         computerScore++;
+        winner = 'Computer';
     }
+    addResult(winner);
 }
 
 function playGame(){
     playRound(getHumanChoice(), getComputerChoice());
     console.log(`Human score: ${humanScore}
     Computer score: ${computerScore}`);
-    
 }
 
-const rockBtn = document.querySelector('#rock');
-const paperBtn = document.querySelector('#paper');
-const scissorBtn = document.querySelector('#scissor');
+function addResult(winner){
+    const result = document.createElement('p');
+    result.textContent = winner;
+    resultBox.appendChild(result);
+}
 
-rockBtn.addEventListener('click', () => {
-    playRound('rock', getComputerChoice());
-});
-paperBtn.addEventListener('click', () => {
-    playRound('paper', getComputerChoice())
-});
-scissorBtn.addEventListener('click', () => {
-    playRound('scissors', getComputerChoice());
-});
